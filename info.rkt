de5a13fa226701d414f1fc245ec62aecbde8f33b
Stephen De Gabrielle <spdegabrielle@gmail.com>
2019-02-24 01:50:53 +0000

    added gstreamer

--- info.rkt
+++ info.rkt
@@ -4,7 +4,13 @@
                "rackunit-lib"
                "draw-lib"
                "gui-lib"
-               "sgl"))
+               "sgl"
+               ("gstreamer-x86_64-macosx" #:platform "x86_64-macosx")
+               ; ("" #:platform "win32\\x86_64")
+               ; ("" #:platform "win32\\i386")
+               ; ("" #:platform "x86_64-linux")
+               ; ("" #:platform "i386-linux")
+               ))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define pkg-desc "Video graphics companion")
 (define version "0.1")
